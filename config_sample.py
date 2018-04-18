cac9e5c1b5ea5c4617bde5915d21044544d3b169
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-04-19 07:57:53 +0900

    add config sample

---
+++ config_sample.py
@@ -0,0 +1,7 @@
+CONFIG_NAME = "pawoo"
+APP_NAME = "utakatasampleapp"
+API_BASE_URL = "https://pawoo.net"
+
+USER_NAME = "yoshikyoto"
+USER_EMAIL = "yoshikyoto@example.co.jp"
+USER_PASSWORD = "password"
