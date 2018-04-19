a87b788c0b139dc6414fc1545f5263947c0a1e12
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-04-19 23:57:35 +0900

    implements streaming api

--- mastodon_api.py
+++ mastodon_api.py
@@ -1,73 +1,32 @@
-from mastodon import Mastodon
+from mastodon import Mastodon, StreamListener
 
 class MastodonApi():
-    """マストドンのAPIを叩くクライアント
+    """MastodonのAPIを叩くためのクライアント"""
 
-    このクライアントを継承して各種パラメータをオーバーライドすることで
-    様々なマストドンインスタンスのAPIを叩くことができる
-    """
-
-    """Mastodonに登録するアプリ名"""
-    app_name = "testapp"
-
-    """Mastodon APIのbase_url
-
-    https://pawoo.net
-    https://friends.nico など
-    継承したクラスでオーバーライドして利用する
-    """
-    api_base_url = ""
-
-    """Mastodonのインスタンス名
-
-    client_idやaccess_token保存のファイル名になるので
-    インスタンスを識別できればよい
-    継承したクラスでオーバーライドして利用する
-    """
-    instance_name = ""
-
-    """ログインユーザーのemail
-
-    継承したクラスでオーバーライドして利用する
-    """
-    user_email = ""
-
-    """ログインユーザーのパスワード
-
-    継承したクラスでオーバーライドして利用する
-    """
-    user_passwprd = ""
-
-    def get_client_id_filename(self):
-        """client_idを保存するファイル名を取得する
-        """
-        return self.app_name + "_" + self.config_name + ".client_id"
-
-    def create_app(self):
-        """Mastodonにアプリを登録してclient_idをファイルに出力する
-        """
-        Mastodon.create_app(
-            self.app_name,
-            api_base_url = self.api_base_url,
-            to_file = self.get_client_id_filename()
+    def __init__(self, client_id_filename, access_token_filename, api_base_url):
+        self.mastodon = Mastodon(
+            client_id = client_id_filename,
+            access_token = access_token_filename,
+            api_base_url = api_base_url
         )
 
-    def get_access_token_filename(self):
-        """access_tokenを保存するファイル名を取得する
-        """
-        return self.app_name + "_" + self.config_name + ".access_token"
+    def toot(self, text):
+        self.mastodon.toot(text)
 
-    def log_in(self):
-        """access_tokenを取得する
+    def stream_local(self):
+        listener = Listener()
+        self.mastodon.stream_local(listener)
 
-        予め対応したclient idを取得してファイルに出力しておく必要がある。
-        """
-        mastodon = Mastodon(
-            client_id = self.get_client_id_filename,
-            api_base_url = self.api_base_url
-        )
-        mastodon.log_in(
-            self.user_email,
-            self.user_password,
-            to_file = self.get_access_token_filename
-        )
+class Listener(StreamListener):
+
+    def on_update(self, status):
+        print(status)
+
+
+if __name__ == "__main__":
+    mastodon = MastodonApi(
+        "utakata_test_app_pawoo.client_id",
+        "utakata_test_app_pawoo.access_token",
+        "https://pawoo.net"
+    )
+    mastodon.stream_local()
